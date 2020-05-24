e14aaea3bf3adce4e65219082019b3eef4d41e4c
Thalles Freitas <thalles.freitas@zup.com.br>
2020-05-24 15:47:00 -0300

    fix test

--- src/test/java/masterdev/br/com/zup/entity/CardTest.java
+++ src/test/java/masterdev/br/com/zup/entity/CardTest.java
@@ -10,23 +10,23 @@ public class CardTest {
 
     @Test
     public void testCard() throws Exception {
-        Assertions.assertEquals(2, new AntivirusCard().getMana());
+        Assertions.assertEquals(2, new AntivirusCard().getManaPoints());
         Assertions.assertEquals(2, new AntivirusCard().getDamage());
-        Assertions.assertEquals(4, new CoffeeCard().getMana());
+        Assertions.assertEquals(4, new CoffeeCard().getManaPoints());
         Assertions.assertEquals(0, new CoffeeCard().getDamage());
-        Assertions.assertEquals(-4, new ChangeExperiencePowerCard().getMana());
+        Assertions.assertEquals(-4, new ChangeExperiencePowerCard().getManaPoints());
         Assertions.assertEquals(6, new ChangeExperiencePowerCard().getDamage());
-        Assertions.assertEquals(-6, new TechLeadPowerCard().getMana());
+        Assertions.assertEquals(-6, new TechLeadPowerCard().getManaPoints());
         Assertions.assertEquals(8, new TechLeadPowerCard().getDamage());
-        Assertions.assertEquals(-3, new FrameworkCard().getMana());
+        Assertions.assertEquals(-3, new FrameworkCard().getManaPoints());
         Assertions.assertEquals(3, new FrameworkCard().getDamage());
-        Assertions.assertEquals(-6, new DebugCard().getMana());
+        Assertions.assertEquals(-6, new DebugCard().getManaPoints());
         Assertions.assertEquals(8, new DebugCard().getDamage());
-        Assertions.assertEquals(-5, new StackTraceCard().getMana());
+        Assertions.assertEquals(-5, new StackTraceCard().getManaPoints());
         Assertions.assertEquals(7, new StackTraceCard().getDamage());
-        Assertions.assertEquals(2, new CoffeeBreakCard().getMana());
+        Assertions.assertEquals(2, new CoffeeBreakCard().getManaPoints());
         Assertions.assertEquals(2, new CoffeeBreakCard().getDamage());
-        Assertions.assertEquals(-4, new StackOverflowCard().getMana());
+        Assertions.assertEquals(-4, new StackOverflowCard().getManaPoints());
         Assertions.assertEquals(4, new StackOverflowCard().getDamage());
     }
 }
