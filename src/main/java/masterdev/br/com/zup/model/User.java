9c4e13b44f0c847abab254ba1e556bcafb58dbdb
Thalles Freitas <thalles.freitas@zup.com.br>
2020-05-24 20:56:45 -0300

    fix duplicate key

--- src/main/java/masterdev/br/com/zup/model/User.java
+++ src/main/java/masterdev/br/com/zup/model/User.java
@@ -4,14 +4,13 @@ import masterdev.br.com.zup.model.players.Player;
 import masterdev.br.com.zup.security.BcryptUtils;
 import masterdev.br.com.zup.security.JwtUtils;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="user_game")
 public class User {
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
     private String nickName;
     private String password;
