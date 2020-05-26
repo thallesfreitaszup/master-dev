eff44c57ae0b8f92b1aae17b8db32488f2bfedc9
Thalles Freitas <thalles.freitas@zup.com.br>
2020-05-25 21:50:16 -0300

    cors

--- src/main/java/masterdev/br/com/zup/controller/GameController.java
+++ src/main/java/masterdev/br/com/zup/controller/GameController.java
@@ -5,13 +5,11 @@ import masterdev.br.com.zup.service.GameService;
 import masterdev.br.com.zup.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/game")
+@CrossOrigin
 public class GameController {
 
     GameService gameService;
--- src/main/java/masterdev/br/com/zup/controller/UserController.java
+++ src/main/java/masterdev/br/com/zup/controller/UserController.java
@@ -9,16 +9,14 @@ import masterdev.br.com.zup.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 
 @RequestMapping("/user")
 @RestController
+@CrossOrigin
 public class UserController {
 
     UserService userService;
